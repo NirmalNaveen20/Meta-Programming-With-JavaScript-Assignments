ebc112ddfc4f359c37ec05b1b0f54e8285e3f085
NirmalNaveen20 <nirmalnaveen95@gmail.com>
2022-12-30 20:36:21 +0530

    final project

---
+++ Little Lemon receipt maker exercise/finalProject.js
@@ -0,0 +1,32 @@
+// Given variables
+const dishData = [
+    {
+        name: "Italian pasta",
+        price: 9.55
+    },
+    {
+        name: "Rice with veggies",
+        price: 8.65
+    },
+    {
+        name: "Chicken with potatoes",
+        price: 15.55
+    },
+    {
+        name: "Vegetarian Pizza",
+        price: 6.45
+    },
+]
+const tax = 1.20;
+
+// Implement getPrices()
+function getPrices() {
+    
+}
+
+// Implement getDiscount()
+function getDiscount() {
+    
+}
+
+// Call getDiscount()
--- Writing a Unit Test/timesTwo.test.js
+++ Writing a Unit Test/timesTwo.test.js
@@ -2,5 +2,5 @@ const timesTwo = require('./timesTwo');
 
 // Write the first test
 test('returns the number times 2', () => {
-    expect().(timesTwo(10)).toBe(20)
+    expect(timesTwo(10)).toBe(20)
 });
